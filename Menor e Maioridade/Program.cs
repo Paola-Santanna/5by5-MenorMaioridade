0f565e6f9b4d00aed842bcc11f0112bd8db3cfa1
Paola-Santanna <paolinhasj@gmail.com>
2024-04-19 15:42:52 -0300

    Add project files.

---
+++ Menor e Maioridade/Program.cs
@@ -0,0 +1,7 @@
+﻿//Faça um programa que receba a idade de 10 pessoas e mostre a quantidade de maiores e menores de idade;
+
+//Início do programa
+//Declaração de variáveis
+//Entrada de dados
+//Operação
+//Final do Programa
